1bc4666e3c1fb71501b382cf631873a469ae460a
anhminh362 <116275383+anhminh362@users.noreply.github.com>
2023-04-23 13:24:57 +0700

    update admin

--- pages/client/ad_schedule.php
+++ pages/client/ad_schedule.php
@@ -1,3 +1,23 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "cinema";
+
+// Create connection
+$conn = mysqli_connect($host, $user, $password, $database);
+mysqli_set_charset($conn, "UTF8");
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+//  echo "Connected Successfully !";
+error_reporting(0);
+
+// 
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -87,7 +107,33 @@
                                     <th>Price</th>
                                     <th>Action</th>
                                 </tr>
-                            </thead>     
+                            </thead>  
+                            <tbody id="tab">
+                                <?php
+                                $sql = "SELECT * FROM schedule";
+                                $sqli = mysqli_query($conn, $sql);
+                                $data=[];
+                                if (mysqli_num_rows($sqli) > 0) {
+                                    while ($row = mysqli_fetch_assoc($sqli)) {
+                                        echo "<tr>";
+                                        echo "<td>" . $row['id'] . "</td>";
+                                        echo "<td><p>" . $row['movie_id'] . "</p></td>";
+                                        echo "<td>" . $row['room_id'] . "</td>";
+                                        echo "<td>" . $row['movie_date'] . "</td>";
+                                        echo "<td>" . $row['time_begin'] . "</td>";
+                                        echo "<td>" . $row['time_end'] . "</td>";
+                                        echo "<td>" . $row['price'] . "</td>";
+                                        
+                                        echo "<td><span data-toggle='modal' data-target='#editModal' class='btn-edit' data-id='" . $row['id'] . "' data-name='" . $row['name'] . "' data-avatar='" . $row['avatar'] . "'data-premiere_date='" . $row['premiere_date'] . "'data-country='" . $row['country'] . "'data-description='" . $row['description'] .
+                                         "'data-trailer='" . $row['trailer'] . "' data-cat='"; echo htmlspecialchars(json_encode($data)); echo "'><ion-icon name='pencil-outline'class='icon-ac-edit'></span><a href='delete.php?id=" . $row['id'] . "'><ion-icon name='trash-outline' class='icon-ac-del'></a>
+                                         <button type='button' class='btn-schedule' data-toggle='modal' data-target='#scheduleModal' data-id='".$row['id']."'> <ion-icon class='icon-ac-add' name='add-circle-outline'></ion-icon></button></td>";
+                                        echo "</tr>";
+                                    }
+                                } else {
+                                    echo "Không có sản phẩm nào.";
+                                }
+                                ?>
+                            </tbody>   
                         </table>
                     </div>
                 </div>
